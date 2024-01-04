f1301bc65f218f38069bc03d2da652969df025ed
billz0824 <youranzhu123@gmail.com>
2024-01-03 22:34:11 -0600

    lol

--- tst/test_pricingutil.cpp
+++ tst/test_pricingutil.cpp
@@ -1,5 +1,51 @@
 #include <gtest/gtest.h>
+#include <pricingutil.h>
 
-TEST(sampleTest, sample) {
-    EXPECT_EQ(4, 4);
+TEST(Tests, accuracyTest) {
+    PricingUtil pricingUtil;
+    float res = pricingUtil.calcVal(2.0, 0.2, 2.0);
+    EXPECT_EQ(pricingUtil.getVal(), res);
 }
+
+TEST(Tests, valueTest) {
+    PricingUtil pricingUtil;
+    float res = pricingUtil.calcVal(2.0, 0.2, 2.0);
+    EXPECT_NEAR(pricingUtil.getVal(), 4.4, 1e-4);
+}
+
+TEST(Tests, zeroInterest) {
+    PricingUtil pricingUtil;
+    float res = pricingUtil.calcVal(2.0, 0.0, 2.0);
+    EXPECT_NEAR(pricingUtil.getVal(), 3.6, 1e-4);
+}
+
+TEST(Tests, negativeInterest) {
+    PricingUtil pricingUtil;
+    float res = pricingUtil.calcVal(2.0, -0.2, 2.0);
+    EXPECT_NEAR(pricingUtil.getVal(), 2.8, 1e-4);
+}
+
+TEST(calcValTest, zeroOleo) {
+    PricingUtil pricingUtil;
+    float res = pricingUtil.calcVal(2.0, 0.2, 0.0);
+    EXPECT_NEAR(pricingUtil.getVal(), 0.0, 1e-4);
+}
+
+TEST(calcValTest, negativeOleo) {
+    PricingUtil pricingUtil;
+    float res = pricingUtil.calcVal(2.0, 0.2, -2.0);
+    EXPECT_NEAR(pricingUtil.getVal(), -4.4, 1e-4);
+}
+
+TEST(calcValTest, zeroPrev) {
+    PricingUtil pricingUtil;
+    float res = pricingUtil.calcVal(0.0, 0.2, 2.0);
+    EXPECT_NEAR(pricingUtil.getVal(), 0.0, 1e-4);
+}
+
+TEST(calcValTest, negativePrev) {
+    PricingUtil pricingUtil;
+    float res = pricingUtil.calcVal(-2.0, 0.2, 2.0);
+    EXPECT_NEAR(pricingUtil.getVal(), -4.4, 1e-4);
+}
+
